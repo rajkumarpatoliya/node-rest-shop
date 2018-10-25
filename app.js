9950d42e433b24f60a21c286be2711d69669ea4a
w4721x <patoliyaraj@gmail.com>
2018-10-25 20:10:16 +0530

    Added new route methods

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 /* jshint esversion: 6 */
 const express = require("express");
 const app = express("./app");
+const morgan = require("morgan");
 
 const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
+app.use(morgan("dev"));
+//Routes handling requests
 app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status(404);
+  next(error);
+});
+
+app.use((req, res, next) => {});
 module.exports = app;
